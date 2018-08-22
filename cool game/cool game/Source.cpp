a8449493ffc7431182d586fc6d45a969cadf6884
unknown <Administrator@BTI-LRODRIGUEZ.nb.org>
2018-08-22 13:27:26 -0400

    added code

---
+++ cool game/cool game/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string name;
+	cout << "Hello\n";
+	cout << "what's your name\n";
+		cin >> name;
+		cout << "\n" << "Hello" << name<<"\n";
+
+		system("PAUSE");
+
+
+}
